1c8d24fdb5b0dec099aecfbbb7f4ee56f71106ee
Riyad <mhriyad000@gmail.com>
2023-06-13 16:46:09 +0600

    daily task

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/pages/launcher_page.dart';
 
+//Riyad Hossain
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
